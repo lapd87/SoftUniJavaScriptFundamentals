0fe10c5f1b9b3903544589e248adf13fa8e9cd09
lapd87 <32339696+lapd87@users.noreply.github.com>
2018-10-28 20:10:53 +0200

    Update _09MonthlyCalendar.js
    fixing broken solution

--- 02OperationAndLogicFlow/_09MonthlyCalendar.js
+++ 02OperationAndLogicFlow/_09MonthlyCalendar.js
@@ -1,19 +1,23 @@
 function calendar([day, month, year]) {
-
-    let input = [day, month, year];
-
+ 
+    let input = [+day, +month, +year];
+ 
     let days = [];
-
+ 
     let prevMonthLastDay = new Date(input[2], input[1] - 1, 0);
-
+ 
+    if (month - 1 < 0) {
+        prevMonthLastDay = new Date(input[2] - 1, 12, 31);
+    }
+ 
     if (prevMonthLastDay.getDay() !== 6) {
         for (let i = prevMonthLastDay.getDay(); i >= 0; i--) {
             days.push(prevMonthLastDay.getDate() - i);
         }
     }
-
+ 
     let currentMonthDays = new Date(input[2], input[1], 0).getDate();
-
+ 
     for (let i = 1; i <= currentMonthDays; i++) {
         if (i === input[0]) {
             days.push(100 + i);
@@ -21,38 +25,37 @@ function calendar([day, month, year]) {
             days.push(i);
         }
     }
-
+ 
     let nextMonthFirstDay = new Date(input[2], input[1], 1);
-
     if (nextMonthFirstDay.getDay() !== 0) {
-        for (let i = 1; i <= nextMonthFirstDay.getDate(); i++) {
+        for (let i = nextMonthFirstDay.getDate(); i <= 6; i++) {
             days.push(i);
         }
     }
-
+ 
     let result = "<table>";
     result += "\r\n";
-    result += "  <tr><th>Sun</th><th>Mon</th><th>Tue</th>" +
+    result += "<tr><th>Sun</th><th>Mon</th><th>Tue</th>" +
         "<th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th></tr>";
     result += "\r\n";
-
+ 
     let isPrevMonth = true;
     let isNextMonth = true;
-
+ 
     let weekCount = days.length / 7;
-
+ 
     for (let week = 1; week <= weekCount; week++) {
         result += "  <tr>";
         for (let day = 1; day <= 7; day++) {
-
+ 
             let currentDay = days[0];
             days.shift();
-
+ 
             if (currentDay === 1 || currentDay === 101) {
                 isPrevMonth = false;
                 isNextMonth = !isNextMonth;
             }
-
+ 
             if (isPrevMonth) {
                 result += `<td class="prev-month">${currentDay}</td>`;
             } else if (isNextMonth) {
@@ -67,10 +70,11 @@ function calendar([day, month, year]) {
         result += "</tr>";
         result += "\r\n";
     }
-
+ 
     result += "</table>";
-
+ 
     console.log(result);
 }
-
-calendar([1, 4, 2016]);
+ 
+// calendar([1, 4, 2016]);
+calendar([24, 12, 2012]);
